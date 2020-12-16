6988c211e941dd1a2e01666e5bac34c8752f4488
angelaleitte <angela-leitte@hotmail.com>
2020-12-15 21:30:43 -0300

    Aula ENTENDENDO OS CONTROLLERS

---
+++ App/controllers/contato.php
@@ -0,0 +1,15 @@
+<?php
+
+class Contato{
+    public function index(){
+        echo "index do contato";
+    }
+
+    public function email(){
+        echo 'angela@leite';
+    }
+
+    public function telefone(){
+        echo "16 9669 6966";
+    }
+}
